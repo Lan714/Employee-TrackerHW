7c189ef2a6fd4a7d6d67be75a68bca8e61b2427c
Lan Bui <lantbui27@gmail.com>
2021-09-13 22:03:27 -0700

    schema sql updated

--- schema.sql
+++ schema.sql
@@ -15,4 +15,12 @@ CREATE TABLE roles (
   FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
-CREATE 
+CREATE TABLE employees (
+  id ( INT AUTO_INCREMENT PRIMARY KEY,
+  first VARCHAR(50) NOT NULL,
+  last VARCHAR(50) NOT NULL,
+  roles_id INT,
+  manager_id INT,
+  FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+  );
--- seed.sql
+++ seed.sql
@@ -3,7 +3,9 @@ USE tracker.db;
 INSERT INTO departments (name)
 VALUES ('Operations'), ('Finance'), ('Human Resources'), ('IT'), ('Marketing');
 
-INSERT INTO roles (name, id, salary, department_id)
+INSERT INTO roles (name, salary, department_id)
 VALUES ()
 
-INSERT INTO employees (id, first, last, role_id, manager_id)
+INSERT INTO employees (first, last, title, roles_id, manager_id)
+VALUES()
+
