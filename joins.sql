b5f63b71b95a7b8717d9df034f59f11eaa578e9b
Lan Bui <lantbui27@gmail.com>
2021-09-13 22:25:50 -0700

    created joins sql

---
+++ joins.sql
@@ -0,0 +1,31 @@
+USE tracker_db;
+
+SELECT roles.id, roles.title AS subject, roles.salary, departments.name AS department
+FROM roles
+INNER JOIN departments
+ON roles.department_id = department_id;
+
+SELECT roles.id, roles.title AS subject, roles.salary, departments.name AS department
+FROM departments
+LEFT JOIN roles
+ON roles.department_id = department_id
+UNION
+SELECT roles.id, roles.title AS subject, roles.salary, departments.name AS department
+FROM departments
+RIGHT JOIN roles
+ON roles.department_id = department_id;
+
+USE tracker_db;
+SELECT CONCAT(employees.first, ' ', employees.last) AS name,
+roles.name AS role,
+subjects.title,
+subjects.salary,
+departments.name AS department,
+CONCAT(manager.first, ' ', manager.last) AS manager
+FROM employees
+LEFT JOIN roles
+ON employees.roles_id = roles_id
+LEFT JOIN departments
+ON roles.department_id = department_id
+LEFT JOIN employees manager
+ON manager.id = employees.manager_id;
