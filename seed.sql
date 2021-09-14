09b96e25d97f449904c3ce70f5edf4db02ba8418
Lan Bui <lantbui27@gmail.com>
2021-09-13 19:15:04 -0700

    created seed sql

---
+++ seed.sql
@@ -0,0 +1,9 @@
+USE tracker.db;
+
+INSERT INTO departments (name)
+VALUES ('Operations'), ('Finance'), ('Human Resources'), ('IT'), ('Marketing');
+
+INSERT INTO roles (name, id, salary, department_id)
+VALUES ()
+
+INSERT INTO employees (id, first, last, role_id, manager_id)
